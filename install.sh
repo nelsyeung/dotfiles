a811ae38e04c2571841aa2d2c47febc3e179cf93
Nelson Yeung <nelson.yeung@darktrace.com>
2024-05-08 12:04:19 +0100

    feat: use symbolic links for installation

--- install.sh
+++ install.sh
@@ -1,58 +1,30 @@
 #!/bin/bash
-
 install() {
-  local cmd=""
-  local exclude=(
-    "*.swp"
-    ".editorconfig"
-    ".git"
-    "LICENSE"
-    "README.md"
-    "install.sh"
-    "profile.ps1"
+  local -r cwd="$(pwd)"
+  local -r files=(
+    .bash_logout
+    .bash_profile
+    .bashrc
+    .general_profile
+    .inputrc
+    .tmux.conf
+    .vim
+    .vimrc
+    .zshrc
   )
-  local exclude_flags=""
-  local msg="These files will be added to or overwritten in the home directory:"
-  local overwrite=""
-  local reply=""
 
-  for p in "${exclude[@]}"; do
-    exclude_flags+="--exclude \"$p\" "
+  for file in ${files[@]}; do
+    ln -sf "$cwd/$file" ~
+    echo "$file installed"
   done
 
-  if [[ -f ~/.system_profile ]]; then
-    exclude_flags+="--exclude \".system_profile\" "
-  fi
-
-  cmd="\rsync -auhP --no-perms $exclude_flags"
-  overwrite="$( \
-    eval $cmd --dry-run . ~ | tail -n +4 | sed '1!G;h;$!d' | tail -n +3 \
-  )"
-
-  if [[ -z "$overwrite" ]]; then
-    echo "Already up-to-date"
-    return
-  fi
-
-  echo "$msg$overwrite"
+  \rsync -auhP .gitconfig .system_profile ~
 
   if [[ $SHELL == *"zsh" ]]; then
-    read "reply?Continue? (y/N) "
+    source ~/.zshrc
   else
-    read -p "Continue? (y/N) " reply
-  fi
-
-  if [[ $reply =~ ^[Yy]$ ]]; then
-    cmd+=". ~"
-    printf "\n$cmd"
-    eval "$cmd"
-
-    if [[ $SHELL == *"zsh" ]]; then
-      source ~/.zshrc
-    else
-      source ~/.bash_profile
-      bind -f ~/.inputrc
-    fi
+    source ~/.bash_profile
+    bind -f ~/.inputrc
   fi
 }
 
